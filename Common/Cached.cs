8fdae9be12f583ffcbdc958d05bf0d0e42b109d9
Cangjier <moodlee@qq.com>
2025-05-11 11:02:32 +0800

    更新 Cached 类以支持可空类型
    将 `_getValue` 更改为 `Func<T?>`，并在返回值时添加空值检查。新增隐式转换操作符以支持 `Func<T?>` 到 `Cached<T>` 的转换。

--- Common/Cached.cs
+++ Common/Cached.cs
@@ -6,14 +6,14 @@
 /// <typeparam name="T"></typeparam>
 public class Cached<T>
 {
-    private readonly Func<T> _getValue;
+    private readonly Func<T?> _getValue;
     private T? _value;
     private bool _isValueCreated = false;
     /// <summary>
     /// 缓存类
     /// </summary>
     /// <param name="getValue"></param>
-    public Cached(Func<T> getValue)
+    public Cached(Func<T?> getValue)
     {
         _getValue = getValue;
     }
@@ -30,7 +30,13 @@ public class Cached<T>
                 _value = _getValue();
                 _isValueCreated = true;
             }
-            return _value!;
+            return _value ?? throw new InvalidOperationException("Value is null");
         }
     }
+
+    /// <summary>
+    /// 隐式转换
+    /// </summary>
+    /// <param name="getValue"></param>
+    public static implicit operator Cached<T> (Func<T?> getValue) => new(getValue);
 }
