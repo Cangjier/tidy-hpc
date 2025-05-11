c69cc8da51ee0546a19878e98bf41a6462ee4fee
Cangjier <moodlee@qq.com>
2025-05-11 10:36:18 +0800

    添加泛型缓存类 Cached<T>
    实现了一个延迟加载和缓存值的泛型类 `Cached<T>`。该类通过构造函数接受一个委托 `getValue`，并在属性 `Value` 中实现缓存逻辑。此外，支持将 `Cached<T>` 实例隐式转换为 `T` 类型。

---
+++ Common/Cached.cs
@@ -0,0 +1,47 @@
+﻿namespace TidyHPC.Common;
+
+/// <summary>
+/// 缓存类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Cached<T>
+{
+    private readonly Func<T> _getValue;
+    private T? _value;
+    private bool _isValueCreated = false;
+    /// <summary>
+    /// 缓存类
+    /// </summary>
+    /// <param name="getValue"></param>
+    public Cached(Func<T> getValue)
+    {
+        _getValue = getValue;
+    }
+
+    /// <summary>
+    /// 获取值
+    /// </summary>
+    public T Value
+    {
+        get
+        {
+            if (!_isValueCreated)
+            {
+                _value = _getValue();
+                _isValueCreated = true;
+            }
+            return _value!;
+        }
+    }
+
+    /// <summary>
+    /// 隐式转换
+    /// </summary>
+    /// <param name="cached"></param>
+    public static implicit operator T(Cached<T> cached)
+    {
+        return cached.Value;
+    }
+
+
+}
